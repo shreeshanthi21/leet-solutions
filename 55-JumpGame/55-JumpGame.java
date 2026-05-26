4c6c9aa561d13fac580f1d292592a003a7372b32
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-05-26 12:33:55 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,14 @@
+// Last updated: 5/26/2026, 12:33:55 PM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;  
+    }
+}
