80902ac2a2fa4470648e3a4538c9e5bb1f5ab152
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-05-26 20:21:53 +0530

    File updated at 5/26/2026, 8:21:52 PM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,32 +1,32 @@
-// Last updated: 5/26/2026, 12:32:14 PM
-class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean[] visited=new boolean[numCourses];
-        boolean[] pathVisited=new boolean[numCourses];
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] edge:prerequisites){
-            adj.get(edge[1]).add(edge[0]);
-        }
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i]){
-                if(dfs(i,-1,adj,visited,pathVisited)) return false;
-            }
-        }
-        return true;
-    }
-    public boolean dfs(int node,int prev,List<List<Integer>> adj,boolean[] visited,boolean[] pathVisited){
-        visited[node]=true;
-        pathVisited[node]=true;
-        for(int neighbor:adj.get(node)){
-            if(!visited[neighbor]){
-                if(dfs(neighbor,node,adj,visited,pathVisited)) return true;
-            }
-            else if(pathVisited[neighbor]) return true;
-        }
-        pathVisited[node]=false;
-        return false;
-    }
-}
+// Last updated: 5/26/2026, 8:21:52 PM
+1class Solution {
+2    public boolean canFinish(int numCourses, int[][] prerequisites) {
+3        boolean[] visited=new boolean[numCourses];
+4        boolean[] pathVisited=new boolean[numCourses];
+5        for(int i=0;i<numCourses;i++){
+6            if(!visited[i]){
+7                if(dfs(i,prerequisites,visited,pathVisited)) return false;
+8            }
+9        }
+10        return true;
+11    }
+12    public boolean dfs(int node, int[][] prerequisites, boolean[] visited, boolean[] pathVisited) {
+13    visited[node] = true;
+14    pathVisited[node] = true;
+15    for (int i = 0; i < prerequisites.length; i++) {
+16        if (prerequisites[i][1] == node) {
+17            int neighbor = prerequisites[i][0];
+18            if (pathVisited[neighbor]) {
+19                return true; 
+20            }
+21            else if (!visited[neighbor]) {
+22                if (dfs(neighbor, prerequisites, visited, pathVisited)) {
+23                    return true;
+24                }
+25            }
+26        }
+27    }
+28    pathVisited[node] = false;
+29    return false;
+30}
+31}
