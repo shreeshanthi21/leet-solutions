7f75079a70239624db341339cb773eca1e64960e
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-05-26 12:34:59 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -0,0 +1,47 @@
+// Last updated: 5/26/2026, 12:34:59 PM
+class Solution {
+    public String longestPalindrome(String str) {
+        if (str.length() <= 1)
+      return str;
+
+    String LPS = "";
+
+    for (int i = 1; i < str.length(); i++) {
+
+      // Consider odd length
+      int low = i;
+      int high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      String palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+
+      // Consider even length
+      low = i-1;
+      high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+    }
+
+    return LPS;
+
+    }
+}
