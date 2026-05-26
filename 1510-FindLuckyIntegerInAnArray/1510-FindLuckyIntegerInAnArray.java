f4003aad794ce56b20fe44d67291186d5d693a11
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-05-26 12:31:17 +0530

    Create 1510-FindLuckyIntegerInAnArray

---
+++ 1510-FindLuckyIntegerInAnArray/1510-FindLuckyIntegerInAnArray.java
@@ -0,0 +1,13 @@
+// Last updated: 5/26/2026, 12:31:17 PM
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] freq=new int[501];
+        for(int i:arr){
+            freq[i]++;
+        }
+        for(int i=500;i>=1;i--){
+            if(freq[i]==i) return i;
+        }
+        return -1;
+    }
+}
