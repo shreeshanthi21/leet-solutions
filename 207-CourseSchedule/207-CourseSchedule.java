6ed423c588f2aa59b80c496dff06415bff75b4b1
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-05-26 20:22:42 +0530

    File updated at 5/26/2026, 8:22:41 PM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,32 +1,32 @@
-// Last updated: 5/26/2026, 8:21:52 PM
+// Last updated: 5/26/2026, 8:22:41 PM
 1class Solution {
 2    public boolean canFinish(int numCourses, int[][] prerequisites) {
 3        boolean[] visited=new boolean[numCourses];
 4        boolean[] pathVisited=new boolean[numCourses];
-5        for(int i=0;i<numCourses;i++){
-6            if(!visited[i]){
-7                if(dfs(i,prerequisites,visited,pathVisited)) return false;
-8            }
-9        }
-10        return true;
-11    }
-12    public boolean dfs(int node, int[][] prerequisites, boolean[] visited, boolean[] pathVisited) {
-13    visited[node] = true;
-14    pathVisited[node] = true;
-15    for (int i = 0; i < prerequisites.length; i++) {
-16        if (prerequisites[i][1] == node) {
-17            int neighbor = prerequisites[i][0];
-18            if (pathVisited[neighbor]) {
-19                return true; 
-20            }
-21            else if (!visited[neighbor]) {
-22                if (dfs(neighbor, prerequisites, visited, pathVisited)) {
-23                    return true;
-24                }
+5        List<List<Integer>> adj=new ArrayList<>();
+6        for(int i=0;i<numCourses;i++){
+7            adj.add(new ArrayList<>());
+8        }
+9        for(int[] edge:prerequisites){
+10            adj.get(edge[1]).add(edge[0]);
+11        }
+12        for(int i=0;i<numCourses;i++){
+13            if(!visited[i]){
+14                if(dfs(i,-1,adj,visited,pathVisited)) return false;
+15            }
+16        }
+17        return true;
+18    }
+19    public boolean dfs(int node,int prev,List<List<Integer>> adj,boolean[] visited,boolean[] pathVisited){
+20        visited[node]=true;
+21        pathVisited[node]=true;
+22        for(int neighbor:adj.get(node)){
+23            if(!visited[neighbor]){
+24                if(dfs(neighbor,node,adj,visited,pathVisited)) return true;
 25            }
-26        }
-27    }
-28    pathVisited[node] = false;
-29    return false;
-30}
+26            else if(pathVisited[neighbor]) return true;
+27        }
+28        pathVisited[node]=false;
+29        return false;
+30    }
 31}
