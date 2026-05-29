8927d693d5207a77dddfbb0ce91cc9f746bd0de8
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-05-29 19:11:15 +0530

    File updated at 5/29/2026, 7:11:14 PM

--- 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.java
+++ 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.java
@@ -1,35 +1,36 @@
-// Last updated: 5/29/2026, 7:10:15 PM
+// Last updated: 5/29/2026, 7:11:14 PM
 1class Solution {
-2    int[] dir=new int[] {0,1,0,-1,0};
-3    public int minimumEffortPath(int[][] heights) {
-4        int m=heights.length;
-5        int n=heights[0].length;
-6        int[][] distance=new int[m][n];
-7        for(int i=0;i<m;i++) Arrays.fill(distance[i],Integer.MAX_VALUE);
-8        distance[0][0]=0;
-9        PriorityQueue<int[]> minheap=new PriorityQueue<>(
-10            Comparator.comparingInt(a->a[0])
-11        );
-12        minheap.offer(new int[]{0,0,0});
-13        while(!minheap.isEmpty()){
-14            int[] curr=minheap.poll();
-15            int dist=curr[0];
-16            int row=curr[1];
-17            int col=curr[2];
-18            if(dist>distance[row][col]) continue;
-19            if(row==m-1 && col==n-1) return dist;
-20            for(int i=0;i<4;i++){
-21                int newRow=row+dir[i];
-22                int newCol=col+dir[i+1];
-23                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
-24                    int newDist=Math.max(dist,Math.abs(heights[newRow][newCol]-heights[row][col]));
-25                    if(distance[newRow][newCol]>newDist){
-26                        distance[newRow][newCol]=newDist;
-27                        minheap.offer(new int[]{newDist,newRow,newCol});
-28                    }
-29                }
-30            } 
-31        }
-32        return 0;
-33    }
-34}
+2    int[] dirR=new int[] {-1,0,+1,0};
+3    int[] dirC=new int[] {0,+1,0,-1};
+4    public int minimumEffortPath(int[][] heights) {
+5        int m=heights.length;
+6        int n=heights[0].length;
+7        int[][] distance=new int[m][n];
+8        for(int i=0;i<m;i++) Arrays.fill(distance[i],Integer.MAX_VALUE);
+9        distance[0][0]=0;
+10        PriorityQueue<int[]> minheap=new PriorityQueue<>(
+11            Comparator.comparingInt(a->a[0])
+12        );
+13        minheap.offer(new int[]{0,0,0});
+14        while(!minheap.isEmpty()){
+15            int[] curr=minheap.poll();
+16            int dist=curr[0];
+17            int row=curr[1];
+18            int col=curr[2];
+19            if(dist>distance[row][col]) continue;
+20            if(row==m-1 && col==n-1) return dist;
+21            for(int i=0;i<4;i++){
+22                int newRow=row+dirR[i];
+23                int newCol=col+dirC[i];
+24                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
+25                    int newDist=Math.max(dist,Math.abs(heights[newRow][newCol]-heights[row][col]));
+26                    if(distance[newRow][newCol]>newDist){
+27                        distance[newRow][newCol]=newDist;
+28                        minheap.offer(new int[]{newDist,newRow,newCol});
+29                    }
+30                }
+31            } 
+32        }
+33        return 0;
+34    }
+35}
