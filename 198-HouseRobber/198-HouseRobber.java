2b17cda6dd8e33fbc34a6a01012e75bf98dfd9e3
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-03 00:16:44 +0530

    File updated at 6/3/2026, 12:16:43 AM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,15 +1,18 @@
-// Last updated: 6/3/2026, 12:10:17 AM
+// Last updated: 6/3/2026, 12:16:43 AM
 1class Solution {
 2    public int rob(int[] nums) {
 3        int n=nums.length;
-4        int[] dp=new int[n+1];
-5        dp[0]=nums[0];
-6        for(int i=1;i<n;i++){
-7            int pick=nums[i];
-8            if(i>1) pick+=dp[i-2];
-9            int nopick=dp[i-1];
-10            dp[i]=Math.max(pick,nopick);
-11        }
-12        return dp[n-1];
-13    }
-14}
+4        int prev=nums[0];
+5        int prev2=0;
+6        int maxi=Integer.MIN_VALUE;
+7        for(int i=1;i<n;i++){
+8            int pick=nums[i];
+9            if(i>1) pick+=prev2;
+10            int nopick=0+prev;
+11            maxi=Math.max(pick,nopick);
+12            prev2=prev;
+13            prev=maxi;
+14        }
+15        return prev;
+16    }
+17}
