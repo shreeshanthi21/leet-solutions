1f675abe185784ce4a451c2266587621775a8bcf
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-03 00:10:19 +0530

    File updated at 6/3/2026, 12:10:17 AM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,18 +1,15 @@
-// Last updated: 6/3/2026, 12:05:44 AM
+// Last updated: 6/3/2026, 12:10:17 AM
 1class Solution {
-2    public int fn(int n,int[] nums,int[] dp){
-3        if(n<0) return 0;
-4        if(n==0) return nums[0];
-5        if(dp[n]!=-1) return dp[n];
-6        int pick=nums[n]+fn(n-2,nums,dp);
-7        int nopick=fn(n-1,nums,dp);
-8        int maxi=Math.max(pick,nopick);
-9        return dp[n]=maxi;
-10    }
-11    public int rob(int[] nums) {
-12        int n=nums.length;
-13        int[] dp=new int[n+1];
-14        Arrays.fill(dp,-1);
-15        return fn(n-1,nums,dp);
-16    }
-17}
+2    public int rob(int[] nums) {
+3        int n=nums.length;
+4        int[] dp=new int[n+1];
+5        dp[0]=nums[0];
+6        for(int i=1;i<n;i++){
+7            int pick=nums[i];
+8            if(i>1) pick+=dp[i-2];
+9            int nopick=dp[i-1];
+10            dp[i]=Math.max(pick,nopick);
+11        }
+12        return dp[n-1];
+13    }
+14}
