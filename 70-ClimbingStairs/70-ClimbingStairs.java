9bf534929b4c6c6c27e6b5b2a36c05a9e82b0c67
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-02 19:57:11 +0530

    File updated at 6/2/2026, 7:57:10 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,14 +1,15 @@
-// Last updated: 5/26/2026, 12:33:43 PM
-class Solution {
-    public int climbStairs(int n) {
-        HashMap<Integer,Integer> dp=new HashMap<>();
-        return climbStairs(n,dp);
-    }
-    private int climbStairs(int n,HashMap<Integer,Integer> dp){
-        if(n<=1) return 1;
-        if(!dp.containsKey(n)){
-            dp.put(n,climbStairs(n-1,dp)+climbStairs(n-2,dp));
-        }
-        return dp.get(n);
-    }
-}
+// Last updated: 6/2/2026, 7:57:10 PM
+1class Solution {
+2
+3    public int fn(int n,int[] dp){
+4        if(n<=1) return 1;
+5        if(dp[n]!=-1) return dp[n];
+6        return dp[n]=fn(n-1,dp)+fn(n-2,dp);
+7    }
+8    public int climbStairs(int n) {
+9       int[] dp=new int[n+1];
+10       Arrays.fill(dp,-1);
+11       return dp[n]=fn(n,dp);
+12
+13    }
+14}
