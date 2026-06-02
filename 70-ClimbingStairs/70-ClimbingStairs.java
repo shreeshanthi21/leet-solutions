380791fbe8343f211bed223e94d769f136efc8b1
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-02 19:59:27 +0530

    File updated at 6/2/2026, 7:59:26 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,15 +1,12 @@
-// Last updated: 6/2/2026, 7:57:10 PM
+// Last updated: 6/2/2026, 7:59:26 PM
 1class Solution {
-2
-3    public int fn(int n,int[] dp){
-4        if(n<=1) return 1;
-5        if(dp[n]!=-1) return dp[n];
-6        return dp[n]=fn(n-1,dp)+fn(n-2,dp);
-7    }
-8    public int climbStairs(int n) {
-9       int[] dp=new int[n+1];
-10       Arrays.fill(dp,-1);
-11       return dp[n]=fn(n,dp);
-12
-13    }
-14}
+2    public int climbStairs(int n) {
+3        int[] dp=new int[n+1];
+4        dp[0]=1;
+5        dp[1]=1;
+6        for(int i=2;i<=n;i++){
+7            dp[i]=dp[i-1]+dp[i-2];
+8        }       
+9        return dp[n];
+10    }
+11}
