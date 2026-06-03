a4cdfc49c95104200dbdbc716af1a648d363e3ea
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-03 19:01:58 +0530

    File updated at 6/3/2026, 7:01:56 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,25 +1,19 @@
-// Last updated: 6/3/2026, 7:01:33 PM
+// Last updated: 6/3/2026, 7:01:56 PM
 1class Solution {
-2    int func(int m, int n, int[][] dp) {
-3        for (int i = 0; i < m; i++) {
-4            for (int j = 0; j < n; j++) {
-5                if (i == 0 && j == 0) {
-6                    dp[i][j] = 1;
-7                    continue; 
-8                }
-9                int up = 0;
-10                int left = 0;
-11                if (i > 0)
-12                    up = dp[i - 1][j];
-13                if (j > 0)
-14                    left = dp[i][j - 1];
-15                dp[i][j] = up + left;
-16            }
-17        }
-18        return dp[m - 1][n - 1];
-19    }
-20    public int uniquePaths(int m, int n) {
-21        int[][] dp = new int[m][n];
-22        return func(m, n, dp);
-23    }
-24}
+2    public int uniquePaths(int m, int n) {
+3        int[] aboveRow = new int[n];
+4        Arrays.fill(aboveRow, 1);
+5
+6        for (int row = 1; row < m; row++) {
+7            int[] currentRow = new int[n];
+8            Arrays.fill(currentRow, 1);
+9            for (int col = 1; col < n; col++) {
+10                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+11            }
+12            aboveRow = currentRow;
+13        }
+14
+15        return aboveRow[n - 1];        
+16
+17    }
+18}
