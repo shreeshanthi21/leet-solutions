4a3063480c4b336280042abec9b488ae9504ccc9
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-03 19:01:35 +0530

    File updated at 6/3/2026, 7:01:33 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,18 +1,25 @@
-// Last updated: 6/3/2026, 6:59:42 PM
+// Last updated: 6/3/2026, 7:01:33 PM
 1class Solution {
-2     private int func(int i, int j, int[][] dp) {
-3        if (i == 0 && j == 0) return 1;
-4        if (i < 0 || j < 0) return 0;
-5        if (dp[i][j] != -1)
-6            return dp[i][j];
-7        int up = func(i - 1, j, dp);
-8        int left = func(i, j - 1, dp);
-9        return dp[i][j] = up + left;
-10    }
-11    public int uniquePaths(int m, int n) {
-12        int dp[][] = new int[m][n];
-13        for (int[] row : dp)
-14            Arrays.fill(row, -1);
-15        return func(m - 1, n - 1, dp);
-16    }
-17}
+2    int func(int m, int n, int[][] dp) {
+3        for (int i = 0; i < m; i++) {
+4            for (int j = 0; j < n; j++) {
+5                if (i == 0 && j == 0) {
+6                    dp[i][j] = 1;
+7                    continue; 
+8                }
+9                int up = 0;
+10                int left = 0;
+11                if (i > 0)
+12                    up = dp[i - 1][j];
+13                if (j > 0)
+14                    left = dp[i][j - 1];
+15                dp[i][j] = up + left;
+16            }
+17        }
+18        return dp[m - 1][n - 1];
+19    }
+20    public int uniquePaths(int m, int n) {
+21        int[][] dp = new int[m][n];
+22        return func(m, n, dp);
+23    }
+24}
