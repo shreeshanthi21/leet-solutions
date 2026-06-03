f8eb40a11ac30dc0062655cf39d865d8622b090f
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-03 18:59:44 +0530

    File updated at 6/3/2026, 6:59:42 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,19 +1,18 @@
-// Last updated: 5/26/2026, 12:33:49 PM
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int[] aboveRow = new int[n];
-        Arrays.fill(aboveRow, 1);
-
-        for (int row = 1; row < m; row++) {
-            int[] currentRow = new int[n];
-            Arrays.fill(currentRow, 1);
-            for (int col = 1; col < n; col++) {
-                currentRow[col] = currentRow[col - 1] + aboveRow[col];
-            }
-            aboveRow = currentRow;
-        }
-
-        return aboveRow[n - 1];        
-
-    }
-}
+// Last updated: 6/3/2026, 6:59:42 PM
+1class Solution {
+2     private int func(int i, int j, int[][] dp) {
+3        if (i == 0 && j == 0) return 1;
+4        if (i < 0 || j < 0) return 0;
+5        if (dp[i][j] != -1)
+6            return dp[i][j];
+7        int up = func(i - 1, j, dp);
+8        int left = func(i, j - 1, dp);
+9        return dp[i][j] = up + left;
+10    }
+11    public int uniquePaths(int m, int n) {
+12        int dp[][] = new int[m][n];
+13        for (int[] row : dp)
+14            Arrays.fill(row, -1);
+15        return func(m - 1, n - 1, dp);
+16    }
+17}
