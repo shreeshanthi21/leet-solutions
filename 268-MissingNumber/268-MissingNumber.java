79f2ff50abd39952ab2cf3618a26a915f114036a
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-04 22:34:42 +0530

    File updated at 6/4/2026, 10:34:42 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,14 +1,12 @@
-// Last updated: 6/4/2026, 10:33:18 PM
+// Last updated: 6/4/2026, 10:34:42 PM
 1class Solution {
 2    public int missingNumber(int[] nums) {
 3        int n=nums.length;
-4        HashSet<Integer> set=new HashSet<>();
-5        for(int num:nums){
-6            set.add(num);
-7        }
-8        for(int i=0;i<n;i++){
-9            if(!set.contains(i)) return i;
-10        }
-11        return n;
-12    }
-13}
+4        int sum=n*(n+1)/2;
+5        int curr=0;
+6        for(int i=0;i<n;i++){
+7            curr+=nums[i];
+8        }
+9        return sum-curr;
+10    }
+11}
