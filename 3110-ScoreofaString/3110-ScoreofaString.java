36af396c99125f8acab325c95c3be35d05e5b73a
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-09 21:33:20 +0530

    File updated at 6/9/2026, 9:33:19 PM

--- 3110-ScoreofaString/3110-ScoreofaString.java
+++ 3110-ScoreofaString/3110-ScoreofaString.java
@@ -1,14 +1,14 @@
-// Last updated: 6/8/2026, 9:55:13 PM
-class Solution {
-    public int scoreOfString(String s) {
-        int ans =0;
-        for(int i =0;i<s.length()-1;i++){
-            char ch = s.charAt(i);
-            char chh = s.charAt(i+1);
-            int a = (int)ch;
-            int b = (int)chh;
-            ans += Math.abs(a-b);
-        }       
-        return ans;
-    }
-}
+// Last updated: 6/9/2026, 9:33:19 PM
+1class Solution {
+2    public int scoreOfString(String s) {
+3        int ans =0;
+4        for(int i =0;i<s.length()-1;i++){
+5            char ch = s.charAt(i);
+6            char chh = s.charAt(i+1);
+7            int a = (int)ch;
+8            int b = (int)chh;
+9            ans += Math.abs(a-b);
+10        }       
+11        return ans;
+12    }
+13}
