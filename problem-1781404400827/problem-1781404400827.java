34519431866832e4dc414d64976318eb11a398bc
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-06-14 08:03:20 +0530

    Create

---
+++ problem-1781404400827/problem-1781404400827.java
@@ -0,0 +1,21 @@
+// Last updated: 6/14/2026, 8:03:20 AM
+1class Solution {
+2    public boolean checkGoodInteger(int n) {
+3        int temp1=n;
+4        int temp2=n;
+5        int digisum=0;
+6        int sqsum=0;
+7        while(temp1!=0){
+8            int rem=temp1%10;
+9            digisum+=rem;
+10            temp1/=10;
+11        }
+12        while(temp2!=0){
+13            int rem=temp2%10;
+14            sqsum+=rem*rem;
+15            temp2/=10;
+16        }
+17        if(sqsum-digisum>=50) return true;
+18        return false;
+19    }
+20}
