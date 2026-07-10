b531c21a8ff5bb699a81f3b6aa3d26496bfa9f18
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-07-10 17:05:40 +0530

    File updated at 7/10/2026, 5:05:40 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,9 +1,9 @@
-// Last updated: 7/10/2026, 5:05:23 PM
+// Last updated: 7/10/2026, 5:05:40 PM
 1class Solution {
 2    public int coinChange(int[] coins, int amount) {
 3        int n=coins.length;
 4        int[][] dp=new int[n+1][amount+1];
-5        
+5        //change in initialization
 6        for(int i=0;i<n+1;i++){
 7            for(int j=0;j<amount+1;j++){
 8                if(j==0) dp[i][j]=0;
