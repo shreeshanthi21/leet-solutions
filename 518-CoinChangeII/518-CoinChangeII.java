ee1570874df29ee74ac7174c63dd870ac6f4ad65
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-07-10 16:49:09 +0530

    File updated at 7/10/2026, 4:49:08 PM

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,19 +1,24 @@
-// Last updated: 6/30/2026, 9:43:57 PM
+// Last updated: 7/10/2026, 4:49:08 PM
 1class Solution {
 2    public int change(int amount, int[] coins) {
-3        int n = coins.length;
-4        int[][] dp = new int[n + 1][amount + 1];
-5        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-6
-7        for (int i = 1; i <= n; i++) {
-8            for (int j = 1; j <= amount; j++) {
-9                if (coins[i - 1] <= j) {
-10                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-11                } else {
-12                    dp[i][j] = dp[i - 1][j];
-13                }
-14            }
-15        }
-16        return dp[n][amount];
-17    }
-18}
+3        int n=coins.length;
+4        int[][] dp=new int[n+1][amount+1];
+5        for(int i=0;i<n+1;i++){
+6            for(int j=0;j<amount+1;j++){
+7                if(i==0) dp[i][j]=0;
+8                if(j==0) dp[i][j]=1;
+9            }
+10        }
+11        for(int i=1;i<n+1;i++){
+12            for(int j=1;j<amount+1;j++){
+13                if(coins[i-1]<=j){
+14                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+15                }
+16                else{
+17                    dp[i][j]=dp[i-1][j];
+18                }
+19            }
+20        }
+21        return dp[n][amount];
+22    }
+23}
