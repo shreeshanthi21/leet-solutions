b6c9ba97e3c20238493cb0f9891fd9ae7c98ec4f
shreeshanthi21 <140417372+shreeshanthi21@users.noreply.github.com>
2026-07-13 19:15:15 +0530

    File updated at 7/13/2026, 7:15:15 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,16 +1,25 @@
-// Last updated: 5/26/2026, 12:31:43 PM
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int sp = 0;
-        int tp = 0;
-
-        while (sp < s.length() && tp < t.length()) {
-            if (s.charAt(sp) == t.charAt(tp)) {
-                sp++;
-            }
-            tp++;
-        }
-
-        return sp == s.length();  
-    }
-}
+// Last updated: 7/13/2026, 7:15:15 PM
+1class Solution {
+2    public boolean isSubsequence(String s, String t) {
+3        int m=s.length();
+4        int n=t.length();
+5        int[][] dp=new int[m+1][n+1];
+6        for(int i=0;i<m+1;i++){
+7            for(int j=0;j<n+1;j++){
+8                if(i==0 || j==0) dp[i][j]=0;
+9            }
+10        }
+11        for(int i=1;i<m+1;i++){
+12            for(int j=1;j<n+1;j++){
+13                if(s.charAt(i-1)==t.charAt(j-1)){
+14                    dp[i][j]=1+dp[i-1][j-1];
+15                }
+16                else{
+17                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+18                }
+19            }
+20        }
+21        if(m!=dp[m][n]) return false;
+22        return true;
+23    }
+24}
